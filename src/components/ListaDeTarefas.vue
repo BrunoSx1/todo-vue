ba465fd6bd7fa203432c74b22152d342419a603a
BrunoSx1 <s.bruno.12@hotmail.com>
2023-04-18 07:24:55 -0300

    adicionando o desafio

--- src/components/ListaDeTarefas.vue
+++ src/components/ListaDeTarefas.vue
@@ -3,15 +3,20 @@ const props = defineProps(['tarefas']);
 </script>
 
 <template>
-    <ul class="list-group-mt-4">
-        <li class="list-group-item" v-for="tarefa in props.tarefas">
-            <input @change="evento => tarefa.finalizada = evento.target.checked" :checked="tarefa.finalizada"
-                :id="tarefa.titulo" type="checkbox">
-            <label :class="{ done: tarefa.finalizada }" class="ms-3" :for="tarefa.titulo">
-                {{ tarefa.titulo }}
-            </label>
-        </li>
-    </ul>
+    <div>
+        <ul class="list-group mt-4" v-if="props.tarefas.length">
+            <li class="list-group-item" v-for="tarefa in props.tarefas" :key="tarefa.titulo">
+                <input @change="evento => tarefa.finalizada = evento.target.checked" :checked="tarefa.finalizada"
+                    :id="tarefa.titulo" type="checkbox" />
+                <label :class="{ done: tarefa.finalizada }" class="ms-3" :for="tarefa.titulo">
+                    {{ tarefa.titulo }}
+                </label>
+            </li>
+        </ul>
+        <div v-else>
+            Sem tarefas pendentes
+        </div>
+    </div>
 </template>
 
 <style scoped>
